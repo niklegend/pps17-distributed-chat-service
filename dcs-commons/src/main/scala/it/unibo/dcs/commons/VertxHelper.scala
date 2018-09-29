e42242a7d3e1c3565fcd8b3b47ace974f06593d4
Mattia Vandi <mattia.vandi@studio.unibo.it>
2018-09-29 16:23:53 +0200

    Simplified VertxHelper.toObservable

--- dcs-commons/src/main/scala/it/unibo/dcs/commons/VertxHelper.scala
+++ dcs-commons/src/main/scala/it/unibo/dcs/commons/VertxHelper.scala
@@ -1,9 +1,8 @@
 package it.unibo.dcs.commons
 
 import io.vertx.core.{AsyncResult, Handler}
-import it.unibo.dcs.commons.RxHelper.Implicits._
 import it.unibo.dcs.commons.VertxHelper.Implicits._
-import rx.functions.Func1
+import RxHelper.Implicits.RxObservable
 import rx.lang.scala.Observable
 
 object VertxHelper {
@@ -11,14 +10,12 @@ object VertxHelper {
   def toObservable[T](action: (AsyncResult[T] => Unit) => Unit): Observable[T] = toObservable[T, T](identity)(action)
 
   def toObservable[J, S](converter: J => S)(action: (AsyncResult[J] => Unit) => Unit): Observable[S] = {
-    val observable = io.vertx.rx.java.RxHelper.observableFuture[J]()
-    action(observable.toHandler())
-    observable.map[S](converter)
+    val javaObservable = io.vertx.rx.java.RxHelper.observableFuture[J]()
+    action(javaObservable.toHandler())
+    RxObservable[J](javaObservable).map[S](converter)
   }
 
   object Implicits {
-
-    implicit def func1ToFunction[T, R](func: Func1[T, R]): Function[T, R] = func.call
 
     implicit def handlerToFunction[T](handler: Handler[T]): Function[T, Unit] = handler.handle
 

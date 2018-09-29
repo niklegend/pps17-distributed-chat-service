1e3b7b32c05c85f28b03c25a48bf3fdce19ff096
Mattia Vandi <mattia.vandi@studio.unibo.it>
2018-09-29 16:19:38 +0200

    Added implicit to covert RxJava Func1 to scala Function1

--- dcs-commons/src/main/scala/it/unibo/dcs/commons/VertxHelper.scala
+++ dcs-commons/src/main/scala/it/unibo/dcs/commons/VertxHelper.scala
@@ -3,6 +3,7 @@ package it.unibo.dcs.commons
 import io.vertx.core.{AsyncResult, Handler}
 import it.unibo.dcs.commons.RxHelper.Implicits._
 import it.unibo.dcs.commons.VertxHelper.Implicits._
+import rx.functions.Func1
 import rx.lang.scala.Observable
 
 object VertxHelper {
@@ -12,10 +13,12 @@ object VertxHelper {
   def toObservable[J, S](converter: J => S)(action: (AsyncResult[J] => Unit) => Unit): Observable[S] = {
     val observable = io.vertx.rx.java.RxHelper.observableFuture[J]()
     action(observable.toHandler())
-    observable.map[S](e => converter(e))
+    observable.map[S](converter)
   }
 
   object Implicits {
+
+    implicit def func1ToFunction[T, R](func: Func1[T, R]): Function[T, R] = func.call
 
     implicit def handlerToFunction[T](handler: Handler[T]): Function[T, Unit] = handler.handle
 

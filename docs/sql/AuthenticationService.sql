a42cf267f877a1a5a1aba91fcea1bf7573f3a83b
AleGnucci <gnucci@live.it>
2018-09-30 19:23:35 +0200

    Added invalid_tokens table (#7)
    * Added invalid_tokens table
    
    * Slight change in AuthenticationService.sql

--- docs/sql/AuthenticationService.sql
+++ docs/sql/AuthenticationService.sql
@@ -1,9 +1,9 @@
 -- *********************************************
 -- * SQL MySQL generation                      
 -- *--------------------------------------------
--- * DB-MAIN version: 9.2.0_32              
--- * Generator date: Apr 23 2015              
--- * Generation date: Tue Sep 25 14:23:02 2018 
+-- * DB-MAIN version: 11.0.0              
+-- * Generator date: Sep  6 2018              
+-- * Generation date: Sun Sep 30 16:41:23 2018 
 -- * LUN file: /Users/mattiavandi/Development/Courses/PPS/pps17-distributed-chat-service/docs/Services.lun 
 -- * Schema: AuthenticationServiceSQL/1 
 -- ********************************************* 
@@ -17,6 +17,12 @@ use AuthService;
 
 -- Tables Section
 -- _____________ 
+
+drop table if exists invalid_tokens;
+create table invalid_tokens (
+     token varchar(2048) not null,
+     expirationDate date not null,
+     constraint id_invalid_token primary key (token));
 
 drop table if exists users;
 create table users (

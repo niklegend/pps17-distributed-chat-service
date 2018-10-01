d61cf0fb226ac28e1e1044943b85b1a7f56e31df
Mattia Vandi <mattia.vandi@studio.unibo.it>
2018-10-01 09:22:18 +0200

    Removed unnecessary space

--- dcs-user-service/src/main/scala/it/unibo/dcs/service/user/GetUserUseCase.scala
+++ dcs-user-service/src/main/scala/it/unibo/dcs/service/user/GetUserUseCase.scala
@@ -7,7 +7,6 @@ import rx.lang.scala.Observable
 final class GetUserUseCase(private[this] val threadExecutor: ThreadExecutor,
                            private[this] val postExecutionThread: PostExecutionThread,
                            private[this] val userRepository: UserRepository)
-
   extends UseCase[User, String](threadExecutor, postExecutionThread) {
 
   override protected[this] def createObservable(username: String): Observable[User] = ???

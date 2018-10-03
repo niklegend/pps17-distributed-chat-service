57f34773d8ca7df770c499c109d3b7955f7d9a59
Mattia Vandi <mattia.vandi@studio.unibo.it>
2018-10-03 19:21:05 +0200

    Written test for CreateUserUseCase

---
+++ dcs-room-service/src/main/scala/it/unibo/dcs/service/room/RoomService.scala
@@ -0,0 +1,30 @@
+package it.unibo.dcs.service.room
+
+import io.vertx.core.{AbstractVerticle, Context, Vertx}
+import io.vertx.scala.ext.web.Router
+import it.unibo.dcs.commons.service.{ServiceVerticle, VertxLogging}
+
+final class RoomService extends ServiceVerticle with VertxLogging {
+
+  private var host: String = _
+  private var port: Int = _
+
+  override def init(jVertx: Vertx, context: Context, verticle: AbstractVerticle): Unit = {
+    super.init(jVertx, context, verticle)
+    val config = context.config
+    host = config.getString("host")
+    port = config.getInteger("port")
+  }
+
+  override protected def initializeRouter(router: Router): Unit = {
+    router.post("/users")
+      .consumes("application/json")
+      .produces("application/json")
+      .handler(rc => ???)
+  }
+
+  override def start(): Unit = startHttpServer(host, port)
+    .subscribe(server => log.info(s"Server started at http://$host:${server.actualPort}"),
+               cause => log.error(s"Could not start server ar http://$host:$port, ${cause.getMessage}"))
+
+}
---
+++ dcs-room-service/src/test/scala/it/unibo/dcs/service/room/interactor/CreateUserUseCaseSpec.scala
@@ -0,0 +1,33 @@
+package it.unibo.dcs.service.room.interactor
+
+import it.unibo.dcs.commons.interactor.executor.{PostExecutionThread, ThreadExecutor}
+import it.unibo.dcs.service.room.repository.RoomRepository
+import it.unibo.dcs.service.room.request.CreateUserRequest
+import org.scalamock.scalatest.MockFactory
+import org.scalatest.{FlatSpec, OneInstancePerTest}
+import rx.lang.scala.{Observable, Subscriber}
+
+final class CreateUserUseCaseSpec extends FlatSpec with MockFactory with OneInstancePerTest {
+
+  val threadExecutor: ThreadExecutor = mock[ThreadExecutor]
+  val postExecutionThread: PostExecutionThread = mock[PostExecutionThread]
+  val roomRepository = mock[RoomRepository]
+  val createUserUseCase = new CreateUserUseCase(threadExecutor, postExecutionThread, roomRepository)
+
+  val subscriber = stub[Subscriber[Unit]]
+
+  val request = CreateUserRequest("mvandi")
+
+  it should "create a new user when the use case is executed" in {
+    // Given
+    (roomRepository createUser _) expects request returns Observable.just()
+
+    // When
+    createUserUseCase(request)
+
+    // Then
+    (subscriber onNext _) verify * once()
+    (subscriber.onCompleted: () => Unit) verify() once()
+  }
+
+}

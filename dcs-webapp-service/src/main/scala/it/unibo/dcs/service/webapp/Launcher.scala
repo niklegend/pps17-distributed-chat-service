e11323b22669379077776613072ba8cbad8f3653
Piscaglia Nicola <nicola.piscaglia2@studio.unibo.it>
2018-10-08 12:08:13 +0200

    implemented webapp-service clustered init

--- dcs-webapp-service/src/main/scala/it/unibo/dcs/service/webapp/Launcher.scala
+++ dcs-webapp-service/src/main/scala/it/unibo/dcs/service/webapp/Launcher.scala
@@ -1,12 +1,19 @@
 package it.unibo.dcs.service.webapp
 
-import io.vertx.scala.core.{DeploymentOptions, Vertx}
+import io.vertx.lang.scala.ScalaLogger
+import io.vertx.scala.core.{DeploymentOptions, Vertx, VertxOptions}
 import it.unibo.dcs.commons.VertxHelper
 import it.unibo.dcs.service.webapp.verticles.WebAppVerticle
+import it.unibo.dcs.commons.VertxHelper.Implicits._
+
 
 object Launcher extends App {
 
-  Vertx vertx() deployVerticle(new WebAppVerticle, getDeploymentOptions)
+  private val logger = ScalaLogger.getLogger(getClass.getName)
+
+  VertxHelper.toObservable[Vertx](Vertx.clusteredVertx(VertxOptions(), _))
+    .subscribe(vertx => vertx deployVerticle(new WebAppVerticle, getDeploymentOptions),
+      cause => logger.error("", cause))
 
   private def getDeploymentOptions = {
     DeploymentOptions() setConfig readServerConfiguration

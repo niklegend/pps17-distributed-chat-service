8b73054d5e3ef67aaa710141794dd72286066fc2
Mattia Vandi <mattia.vandi@studio.unibo.it>
2018-10-14 16:57:01 +0200

    Fixed closing of add-room

--- dcs-client/src/app/add-room/add-room.component.ts
+++ dcs-client/src/app/add-room/add-room.component.ts
@@ -1,29 +1,32 @@
-import { Component, Inject, OnInit, Input } from '@angular/core';
-import {ChatComponent} from '../chat/chat.component';
-import { CreateRoomRequest } from '../requests';
+import { Component, Inject, OnInit, Input } from "@angular/core";
+import { ChatComponent } from "../chat/chat.component";
+import { CreateRoomRequest } from "../requests";
 
 @Component({
-  selector: 'app-add-room',
-  templateUrl: './add-room.component.html',
-  styleUrls: ['./add-room.component.css']
+  selector: "app-add-room",
+  templateUrl: "./add-room.component.html",
+  styleUrls: ["./add-room.component.css"]
 })
 export class AddRoomComponent implements OnInit {
-
   @Input()
   name: string = null;
 
-  constructor(@Inject(ChatComponent) private parent: ChatComponent) { }
+  constructor(@Inject(ChatComponent) private parent: ChatComponent) {}
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
   createRoom() {
     if (this.name == null) {
-        console.log("Insert a name to add new room!");
+      console.log("Insert a name to add new room!");
     } else {
       console.log("Create new Room: " + this.name);
-      this.parent.createRoom(new CreateRoomRequest(this.name, this.parent.getUser().username, "token"));
-      this.name = null;
+      this.parent.createRoom(
+        new CreateRoomRequest(
+          this.name,
+          this.parent.getUser().username,
+          this.parent.getUser().token
+        ));
+      this.name = "";
     }
   }
 }
--- dcs-client/src/app/chat/chat.component.ts
+++ dcs-client/src/app/chat/chat.component.ts
@@ -1,29 +1,27 @@
-import { Component, OnInit } from '@angular/core';
-import { remove } from 'lodash';
+import { Component, OnInit } from "@angular/core";
+import { remove } from "lodash";
 
-import { ChatService } from '../chat.service';
-import { DeleteRoomRequest, CreateRoomRequest } from '../requests';
-import { Room, User} from '../model';
+import { ChatService } from "../chat.service";
+import { DeleteRoomRequest, CreateRoomRequest } from "../requests";
+import { Room, User } from "../model";
 
 @Component({
-  selector: 'app-chat',
-  templateUrl: './chat.component.html',
-  styleUrls: ['./chat.component.css']
+  selector: "app-chat",
+  templateUrl: "./chat.component.html",
+  styleUrls: ["./chat.component.css"]
 })
 export class ChatComponent implements OnInit {
-
   rooms: Room[];
 
   create = false;
 
-  constructor(private service: ChatService) {
-  }
+  constructor(private service: ChatService) {}
 
   ngOnInit() {
     // to debug
     this.rooms = [];
     this.rooms.push({
-      name: 'Room 1',
+      name: "Room 1",
       participations: []
     });
   }
@@ -33,20 +31,27 @@ export class ChatComponent implements OnInit {
   }
 
   deleteRoom(room: Room) {
-    this.service.deleteRoom(new DeleteRoomRequest(room.name, 'TODO: replace with username', 'TODO: replace with token'))
-      .subscribe(
-        deletedRoom => remove(this.rooms, r => r.name === deletedRoom.name)
+    this.service
+      .deleteRoom(
+        new DeleteRoomRequest(
+          room.name,
+          this.service.getUser().username,
+          this.service.getUser().token
+        )
+      )
+      .subscribe(deletedRoom =>
+        remove(this.rooms, r => r.name === deletedRoom.name)
       );
   }
 
   createRoom(request: CreateRoomRequest) {
-    this.service.createRoom(request).subscribe(
-      partecipation => this.rooms.push(partecipation.room)
-    );
+    this.service.createRoom(request).subscribe(partecipation => {
+      this.create = false;
+      this.rooms.push(partecipation.room);
+    });
   }
 
   toggleCreate() {
     this.create = !this.create;
   }
-
 }

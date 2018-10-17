ebcf39e88a51048a0c40829f492ee0ed8396752f
A_Gnucci <alessandro.gnucci@studio.unibo.it>
2018-10-17 12:29:09 +0200

    Removed duplicate classes in Authentication Service and enabled rest communication in WebApp service

--- dcs-webapp-service/src/main/scala/it/unibo/dcs/service/webapp/Launcher.scala
+++ dcs-webapp-service/src/main/scala/it/unibo/dcs/service/webapp/Launcher.scala
@@ -1,7 +1,9 @@
 package it.unibo.dcs.service.webapp
 
+import com.hazelcast.config.Config
 import io.vertx.lang.scala.ScalaLogger
-import io.vertx.scala.core.{Vertx, VertxOptions}
+import io.vertx.scala.core.Vertx
+import io.vertx.spi.cluster.hazelcast.HazelcastClusterManager
 import it.unibo.dcs.commons.VertxHelper
 import it.unibo.dcs.commons.VertxHelper.Implicits._
 import it.unibo.dcs.service.webapp.verticles.WebAppVerticle
@@ -15,8 +17,12 @@ object Launcher extends App {
 
   private val logger = ScalaLogger.getLogger(getClass.getName)
 
-  VertxHelper.toObservable[Vertx](Vertx.clusteredVertx(VertxOptions(), _))
+  private val hazelcastConfig = new Config()
+  hazelcastConfig.getProperties.setProperty("hazelcast.rest.enabled", "true")
+  private val manager = new HazelcastClusterManager(hazelcastConfig)
+  private val options = io.vertx.scala.core.VertxOptions(new io.vertx.core.VertxOptions().setClusterManager(manager))
+
+  VertxHelper.toObservable[Vertx](Vertx.clusteredVertx(options, _))
     .subscribe(vertx => vertx deployVerticle(new WebAppVerticle, deploymentOptions),
       cause => logger.error("", cause))
-
 }

b758df8d47fb9b09b1cacdbfe8a6fcf1eb72bac4
Mattia Vandi <mattia.vandi@studio.unibo.it>
2018-10-24 13:59:42 +0200

    Added consumer, publisher and subscriber abstraction

--- dcs-webapp-service/src/main/scala/it/unibo/dcs/service/webapp/verticles/handler/impl/subscribers/RoomCreationSubscriber.scala
+++ dcs-webapp-service/src/main/scala/it/unibo/dcs/service/webapp/verticles/handler/impl/subscribers/RoomCreationSubscriber.scala
@@ -1,20 +1,18 @@
 package it.unibo.dcs.service.webapp.verticles.handler.impl.subscribers
 
-
 import io.netty.handler.codec.http.HttpResponseStatus
 import io.vertx.lang.scala.ScalaLogger
 import io.vertx.lang.scala.json.{Json, JsonObject}
 import io.vertx.scala.core.http.HttpServerResponse
-import it.unibo.dcs.commons.Address
+import it.unibo.dcs.commons.{Address, Publisher}
 import it.unibo.dcs.commons.VertxWebHelper.Implicits.{RichHttpServerResponse, jsonObjectToString}
 import it.unibo.dcs.exceptions._
 import it.unibo.dcs.service.webapp.interaction.Results.Implicits._
 import it.unibo.dcs.service.webapp.interaction.Results.RoomCreationResult
 import rx.lang.scala.Subscriber
 
-
 final class RoomCreationSubscriber(protected override val response: HttpServerResponse,
-                                   private[this] val address: Address) extends Subscriber[RoomCreationResult]
+                                   private[this] val publisher: Publisher) extends Subscriber[RoomCreationResult]
   with ErrorSubscriber {
 
   private val log = ScalaLogger.getLogger(getClass.getName)
@@ -26,13 +24,14 @@ final class RoomCreationSubscriber(protected override val response: HttpServerRe
 
     log.info(s"Broadcasting: $json")
 
-    address.publish[JsonObject](json)
+    publisher.publish[JsonObject](json)
   }
 
 }
 
 object RoomCreationSubscriber {
 
-  def apply(response: HttpServerResponse, address: Address): RoomCreationSubscriber = new RoomCreationSubscriber(response, address)
+  def apply(response: HttpServerResponse, publisher: Publisher): RoomCreationSubscriber =
+    new RoomCreationSubscriber(response, publisher)
 
 }
--- dcs-webapp-service/src/main/scala/it/unibo/dcs/service/webapp/verticles/handler/impl/subscribers/RoomDeletionSubscriber.scala
+++ dcs-webapp-service/src/main/scala/it/unibo/dcs/service/webapp/verticles/handler/impl/subscribers/RoomDeletionSubscriber.scala
@@ -2,26 +2,26 @@ package it.unibo.dcs.service.webapp.verticles.handler.impl.subscribers
 
 import io.vertx.lang.scala.json.Json
 import io.vertx.scala.core.http.HttpServerResponse
-import it.unibo.dcs.commons.Address
+import it.unibo.dcs.commons.{Address, Publisher}
 import it.unibo.dcs.exceptions._
 import rx.lang.scala.Subscriber
 
 import scala.language.postfixOps
 
 final class RoomDeletionSubscriber(protected override val response: HttpServerResponse,
-                                   private[this] val address: Address) extends Subscriber[String]
+                                   private[this] val publisher: Publisher) extends Subscriber[String]
   with ErrorSubscriber {
 
   override def onNext(name: String): Unit = {
     response.end()
-    address.publish(Json.obj(("name", name)))
+    publisher.publish(Json.obj(("name", name)))
   }
 
 }
 
 object RoomDeletionSubscriber {
 
-  def apply(response: HttpServerResponse, address: Address): RoomDeletionSubscriber =
-    new RoomDeletionSubscriber(response, address)
+  def apply(response: HttpServerResponse, publisher: Publisher): RoomDeletionSubscriber =
+    new RoomDeletionSubscriber(response, publisher)
 
 }

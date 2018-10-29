e4effe5c733308a1d3071466a0ec1f9760a61b69
Mattia Vandi <mattia.vandi@studio.unibo.it>
2018-10-29 17:24:28 +0100

    Added explanatory comment

--- dcs-commons/src/main/scala/it/unibo/dcs/commons/Logging.scala
+++ dcs-commons/src/main/scala/it/unibo/dcs/commons/Logging.scala
@@ -7,6 +7,7 @@ trait Logging {
   private[this] var _log: ScalaLogger = _
 
   protected def log: ScalaLogger = {
+    // Double-checked locking (https://en.wikipedia.org/wiki/Double-checked_locking)
     if (_log == null) {
       synchronized {
         if (_log == null) {

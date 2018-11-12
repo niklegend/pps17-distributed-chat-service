bd5b9b6d257c04e11e20eba8fb1a9d85cf1f0e55
A_Gnucci <alessandro.gnucci@studio.unibo.it>
2018-11-12 18:53:01 +0100

    Small code quality changes in Authentication Service

--- dcs-authentication-service/src/main/scala/it/unibo/dcs/service/authentication/business_logic/JwtTokenGenerator.scala
+++ dcs-authentication-service/src/main/scala/it/unibo/dcs/service/authentication/business_logic/JwtTokenGenerator.scala
@@ -1,31 +1,22 @@
 package it.unibo.dcs.service.authentication.business_logic
 
 import java.util.Date
-
 import io.vertx.lang.scala.json.Json
 import io.vertx.scala.ext.auth.jwt.{JWTAuth, JWTOptions}
 
-/** Utility class useful to generate jwt tokens */
-class JwtTokenGenerator(jwtAuth: JWTAuth) {
+/** Utility object useful to generate jwt tokens */
+object JwtTokenGenerator extends ((JWTAuth, String) => String){
 
   /**
     * It generates a token, given a username
     *
+    * @param jwtAuth the vertx jwt authentication provider
     * @param username the username of the user
     * @return a jwt token */
-  def generateToken(username: String): String = {
+  def apply(jwtAuth: JWTAuth, username: String): String = {
     val MAXIMUM_TOKEN_AGE = 300 // in minutes, it is equal to 5 hours
     jwtAuth.generateToken(Json.obj(("sub", username), ("iat", new Date().toInstant.toEpochMilli / 1000)),
       JWTOptions().setExpiresInMinutes(MAXIMUM_TOKEN_AGE)) //expires after 5 hours
   }
 
-}
-
-object JwtTokenGenerator {
-
-  /** Factory method to instantiate the class
-    *
-    * @param jwtAuth jwt authentication provider
-    * @return an instantiation of the class */
-  def apply(jwtAuth: JWTAuth): JwtTokenGenerator = new JwtTokenGenerator(jwtAuth)
 }
--- dcs-authentication-service/src/main/scala/it/unibo/dcs/service/authentication/interactor/usecases/ReturningTokenUseCase.scala
+++ dcs-authentication-service/src/main/scala/it/unibo/dcs/service/authentication/interactor/usecases/ReturningTokenUseCase.scala
@@ -1,12 +1,7 @@
 package it.unibo.dcs.service.authentication.interactor.usecases
 
 import io.vertx.scala.ext.auth.jwt.JWTAuth
-import it.unibo.dcs.commons.interactor.UseCase
-import it.unibo.dcs.commons.interactor.executor.{PostExecutionThread, ThreadExecutor}
 import it.unibo.dcs.service.authentication.business_logic.JwtTokenGenerator
-import it.unibo.dcs.service.authentication.repository.AuthenticationRepository
-import it.unibo.dcs.service.authentication.request.Requests.TokenRequest
-import rx.lang.scala.Observable
 
 /** It represents any use case that should return a jwt token */
 trait ReturningTokenUseCase {
@@ -17,6 +12,6 @@ trait ReturningTokenUseCase {
     * @param jwtAuth the jwt authentication provider
     * */
   def createToken(username: String, jwtAuth: JWTAuth): String =
-    JwtTokenGenerator(jwtAuth).generateToken(username)
+    JwtTokenGenerator(jwtAuth, username)
 
 }
